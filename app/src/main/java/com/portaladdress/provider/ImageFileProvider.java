e1937b9d2d8784939d9526cfaa03773665f712f3
Ezequiel Alécio Ribeiro <ezequiel.ribeiro@gmail.com>
2022-07-04 08:33:29 -0300

    Update ImageFileProvider.java

--- app/src/main/java/com/portaladdress/provider/ImageFileProvider.java
+++ app/src/main/java/com/portaladdress/provider/ImageFileProvider.java
@@ -3,4 +3,7 @@ package com.portaladdress.provider;
 import androidx.core.content.FileProvider;
 
 public class ImageFileProvider extends FileProvider {
+   public ImageFileProvider() {
+       super(R.xml.filepath)
+   }
 }
