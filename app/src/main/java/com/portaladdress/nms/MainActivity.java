ff179ea1301350dcf0851a4d96ae903e7226f103
Ezequiel Alécio Ribeiro <ezequiel.ribeiro@gmail.com>
2025-12-05 21:54:30 -0300

    v1.22

--- app/src/main/java/com/portaladdress/nms/MainActivity.java
+++ app/src/main/java/com/portaladdress/nms/MainActivity.java
@@ -40,6 +40,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
@@ -56,6 +57,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.appupdate.AppUpdateOptions;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
@@ -66,6 +75,7 @@ import com.portaladdress.nms.ui.main.SectionsPagerAdapter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements PurchasesUpdatedListener {
 
@@ -108,23 +118,195 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
             }
         });*/
 
-        int rated = getSharedPreferences("rated", MODE_PRIVATE).getInt("time", 0);
+      /*  int rated = getSharedPreferences("rated", MODE_PRIVATE).getInt("time", 0);
         getSharedPreferences("rated", MODE_PRIVATE).edit().putInt("time", rated + 1).commit();
 
         if (rated == 5) {
             showRequestRateApp(MainActivity.this);
         } else if (rated == 30) {
             getSharedPreferences("rated", MODE_PRIVATE).edit().putInt("time", 0).commit();
+        }*/
+
+        /*RequestConfiguration requestConfiguration = new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("CA2976559C9489A7C3F8367D5C73ABE6")).build();
+        MobileAds.setRequestConfiguration(requestConfiguration);*/
+        try {
+            rateApp(MainActivity.this);
+        } catch (Exception e) {
+            Log.e("NMS","error rateApp()");
         }
-
-        RequestConfiguration requestConfiguration = new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("CA2976559C9489A7C3F8367D5C73ABE6")).build();
-        MobileAds.setRequestConfiguration(requestConfiguration);
-
-        billingSetup();
+        checkUpdate(MainActivity.this);
 
     }
 
-    private void billingSetup() {
+    private void checkUpdate(Context context) {
+
+        Activity activity = (Activity) context;
+
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
+
+// Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        appUpdateInfoTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("checkUpdate" + e.getMessage());
+
+            }
+        });
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                appUpdateManager.startUpdateFlow(appUpdateInfo, activity, new AppUpdateOptions() {
+                    @Override
+                    public int appUpdateType() {
+                        return AppUpdateType.IMMEDIATE;
+                    }
+
+                    @Override
+                    public boolean allowAssetPackDeletion() {
+                        return false;
+                    }
+                });
+            } else {
+                System.out.println("App is updated");
+
+            }
+        });
+    }
+
+    @Override
+    public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> purchases) {
+
+
+        if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
+            for (Purchase purchase : purchases) {
+                verifySubPurchase(purchase);
+            }
+        }
+    }
+
+    void establishConnection() {
+
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Handle disconnection if necessary
+            }
+
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    // Query existing purchases and update premium status
+                    showProducts();
+                    billingClient.queryPurchasesAsync(
+                            QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.INAPP).build(),
+                            (billingResult1, list) -> {
+                                if (billingResult1.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                                    if (!list.isEmpty()) {
+                                        // Activate premium feature
+                                        sharedPreferences.edit().putBoolean("enableAd", false).apply();
+                                        // Process each purchase if needed
+                                    } else {
+                                        // De-activate premium feature
+                                        sharedPreferences.edit().putBoolean("enableAd", true).apply();
+                                    }
+                                }
+                            }
+                    );
+                }
+            }
+        });
+
+
+    }
+
+    void verifySubPurchase(Purchase purchase) {
+        // Acknowledge the purchase
+        AcknowledgePurchaseParams acknowledgePurchaseParams = AcknowledgePurchaseParams
+                .newBuilder()
+                .setPurchaseToken(purchase.getPurchaseToken())
+                .build();
+
+        billingClient.acknowledgePurchase(acknowledgePurchaseParams, billingResult -> {
+            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                // Subscription activated, update UI or perform necessary actions
+                Toast.makeText(MainActivity.this, "Subscription activated, Enjoy!", Toast.LENGTH_SHORT).show();
+                sharedPreferences.edit().putBoolean("enableAd", false).apply(); // Set premium status to 1
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    void showProducts() {
+        // Define your product details
+        ImmutableList<QueryProductDetailsParams.Product> productList = ImmutableList.of(
+                // Product 1
+              /*  QueryProductDetailsParams.Product.newBuilder()
+                        .setProductId("one_week")
+                        .setProductType(BillingClient.ProductType.SUBS)
+                        .build(),
+                // Product 2
+                QueryProductDetailsParams.Product.newBuilder()
+                        .setProductId("one_month")
+                        .setProductType(BillingClient.ProductType.SUBS)
+                        .build(),*/
+                // Product 3
+                QueryProductDetailsParams.Product.newBuilder()
+                        .setProductId("removead1")
+                        .setProductType(BillingClient.ProductType.INAPP)
+                        .build()
+        );
+
+        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
+                .setProductList(productList)
+                .build();
+
+        // Query product details asynchronously
+        billingClient.queryProductDetailsAsync(params, (billingResult, prodDetailsList) -> {
+
+            productDetails = prodDetailsList.getProductDetailsList().get(0);
+
+
+        });
+    }
+
+
+    void launchPurchaseFlow() {
+        BillingFlowParams billingFlowParams =
+                BillingFlowParams.newBuilder()
+                        .setProductDetailsParamsList(
+                                ImmutableList.of(
+                                        BillingFlowParams.ProductDetailsParams.newBuilder()
+                                                .setProductDetails(productDetails)
+                                                .build()
+                                )
+                        )
+                        .build();
+
+        billingClient.launchBillingFlow(this, billingFlowParams);
+    }
+
+    void checkSubscription() {
+        billingClient  = BillingClient.newBuilder(getApplicationContext())
+                .setListener(this)
+                .enablePendingPurchases(PendingPurchasesParams.newBuilder()
+                        .enableOneTimeProducts() // Enable pending purchases for one-time products
+                        // .enablePrepaidPlans() // Optionally, enable pending purchases for prepaid plans
+                        .build())
+                .build();
+
+        establishConnection();
+
+
+    }
+
+
+ /*   private void billingSetup() {
 
         billingClient = BillingClient.newBuilder(this)
                 .setListener(this)
@@ -306,7 +488,7 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
             sharedPreferences.edit().putBoolean("enableAd", true).apply();
             Log.i("billingResult", "ITEM_NOT_OWNED");
         }
-    }
+    }*/
 
     private void loadAd() {
         mAdView = findViewById(R.id.adView);
@@ -374,7 +556,7 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
 
     }
 
-    private void showRequestRateApp(final Activity activity) {
+  /*  private void showRequestRateApp(final Activity activity) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle("Feedback");
         builder.setMessage("Could you please qualify our app?");
@@ -397,47 +579,54 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
         });
         AlertDialog dialog = builder.create();
         dialog.show();
-    }
+    }*/
 
     private void rateApp(final Activity activity) throws Exception {
-        final ReviewManager reviewManager = ReviewManagerFactory.create(activity);
+        final ReviewManager reviewManager = ReviewManagerFactory.create(MainActivity.this);
         //reviewManager = new FakeReviewManager(this);
         Task<ReviewInfo> request = reviewManager.requestReviewFlow();
 
         request.addOnCompleteListener(new OnCompleteListener<ReviewInfo>() {
             @Override
-            public void onComplete(Task<ReviewInfo> task) {
+            public void onComplete(@NonNull Task<ReviewInfo> task) {
+
                 if (task.isSuccessful()) {
                     ReviewInfo reviewInfo = task.getResult();
-                    Task<Void> flow = reviewManager.launchReviewFlow(activity, reviewInfo);
+                    Task<Void> flow = reviewManager.launchReviewFlow(MainActivity.this, reviewInfo);
+
                     flow.addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
-                        public void onComplete(Task<Void> task) {
-                            Log.e("Rate Flow", "Complete");
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.i("Rate Flow", "Result: "+task.isComplete());
                         }
                     });
 
                     flow.addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(Exception e) {
-                            activity.getSharedPreferences("rated", MODE_PRIVATE).edit().putInt("time", 0).commit();
-                            Log.e("Rate Flow", "Fail");
-                            e.printStackTrace();
+                            Log.i("Rate Flow", "Fail");
+                            System.err.println(e);
                         }
                     });
 
                 } else {
-                    activity.getSharedPreferences("rated", MODE_PRIVATE).edit().putInt("time", 0).commit();
-                    Log.e("Rate Task", "Fail");
+                    try {
+                        String reviewErrorCode = Objects.requireNonNull(task.getException()).getMessage();
+                        Log.d("Rate Task Fail", "cause: " + reviewErrorCode);
+
+                    } catch (NullPointerException | ClassCastException e) {
+                        System.err.println(e.getMessage());
+
+                    }
                 }
             }
 
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(Exception e) {
-                activity.getSharedPreferences("rated", MODE_PRIVATE).edit().putInt("time", 0).commit();
-                e.printStackTrace();
-                Log.e("Rate Request", "Fail");
+                System.err.println(e.getMessage());
+                Log.d("Rate Request", "Fail");
+
             }
         });
 
@@ -581,7 +770,7 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
 
             case R.id.menuRate:
                 try {
-                    rateApp(MainActivity.this);
+                    rateAppOnPlayStore();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -594,7 +783,7 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
 
             case R.id.menuRemoveAd:
                 try {
-                    makePurchase();
+                    launchPurchaseFlow();
                 }catch (NullPointerException e){e.printStackTrace();}
                 break;
 
@@ -608,7 +797,14 @@ public class MainActivity extends AppCompatActivity implements PurchasesUpdatedL
     }
 
 
-
+    private void rateAppOnPlayStore(){
+        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+    }
 
    
 }
